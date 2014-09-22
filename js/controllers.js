d03a75def0ef5dd2c0816c34554ce41e83a35cb7
Zack Sheppard <z@zack.io>
2014-09-22 17:34:59 -0400

    dribbble callback working. continue at 19:39 in video

--- js/controllers.js
+++ js/controllers.js
@@ -6,3 +6,10 @@ controllers.controller('AppCtrl', function ($scope){
   $scope.name = "Module";
 });
 
+controllers.controller('ShotsListCtrl', function($scope, $http){
+  $scope.list;
+  $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+      console.log(data);
+  })
+})
+
