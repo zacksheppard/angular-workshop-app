d6717f9a2515c4e7c3180b6d7791dfa9a36e6277
Zack Sheppard <z@zack.io>
2014-09-23 19:55:32 -0400

    list working and styled

--- js/controllers.js
+++ js/controllers.js
@@ -9,6 +9,7 @@ controllers.controller('AppCtrl', function ($scope){
 controllers.controller('ShotsListCtrl', function($scope, $http){
   $scope.list;
   $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+      $scope.list = data.data;
       console.log(data);
   })
 })
