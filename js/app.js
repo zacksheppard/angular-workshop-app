701764a96a099c3950c734550e148c0bb4ab5dfa
Zack Sheppard <z@zack.io>
2014-09-24 19:14:34 -0400

    complete through 31:47

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 "use strict";
 
 var app = angular.module('dabbble', ['dabbble.controllers']);
+
+app.config(function ($routeProvider)){
+  $routeProvider.when("/:list", {controller:"ShotsListCtrl", templateUrl: "partials/shots/shots_list.html"})
+}); 
