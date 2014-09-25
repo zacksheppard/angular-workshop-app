7c8e1c900ec1e02ba37f54228820934e2fd1d9e3
Zack Sheppard <z@zack.io>
2014-09-25 12:26:57 -0400

    adds popular, everyone, and debuts list and links

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@
 
 var app = angular.module('dabbble', ['dabbble.controllers']);
 
-app.config(function ($routeProvider)){
-  $routeProvider.when("/:list", {controller:"ShotsListCtrl", templateUrl: "partials/shots/shots_list.html"})
+app.config(function ($routeProvider){
+  $routeProvider
+  .when("/:list", {controller:"ShotsListCtrl", templateUrl: "partials/shots_list.html"})
+  .when("/shots/:id", {controller:"ShotsCtrl", templateUrl: "partials/shot.html"})
+  // .otherwise({redirectTo: "/popular"})
 }); 
--- js/controllers.js
+++ js/controllers.js
@@ -6,11 +6,16 @@ controllers.controller('AppCtrl', function ($scope){
   $scope.name = "Module";
 });
 
-controllers.controller('ShotsListCtrl', function($scope, $http){
+controllers.controller('ShotsListCtrl', function($scope, $routeParams, $http) {
+
+  var list = $routeParams.list;
   $scope.list;
-  $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+  $http.jsonp('http://api.dribbble.com/shots/' + list + '?callback=JSON_CALLBACK').then(function(data){
       $scope.list = data.data;
       console.log(data);
   })
-})
+});
 
+controllers.controller('ShotsCtrl', function($scope){
+  
+});
